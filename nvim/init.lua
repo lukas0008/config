e6f592023568a636fb7b2d1982941c5c7dd91f35
lukas0008 <me@zajac.lu>
2026-03-30 11:02:27 +0200

    stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,15 +19,29 @@ vim.lsp.inlay_hint.enable(true)
 -- vim.lsp.enable("checkstyle")
 
 vim.lsp.config["asm-lsp"] = {
-	cmd = { "asm-lsp" },
-	filetypes = { "asm", "s", "S" },
+  cmd = { "asm-lsp" },
+  filetypes = { "asm", "s", "S" },
 }
-
+  
 vim.lsp.enable("asm-lsp")
 vim.lsp.enable("tinymist")
 vim.lsp.enable("zls")
 vim.lsp.config('zls', {})
 vim.lsp.enable("jdtls")
+-- vim.filetype.add {
+--   extension = {
+--     c3 = "c3"
+--   }
+-- }
+-- local lsputil = require('lspconfig/util')
+-- vim.lsp.config('c3-lsp', {
+--   cmd = { "c3lsp" },
+--   filetypes = { "c3" },
+--     -- root_dir = function(fname)
+--     --   -- return lsputil.find_git_ancestor(fname)
+--     -- end
+--   })
+-- vim.lsp.enable("c3-lsp")
 vim.filetype.add {
   extension = {
     nu = "nu"
@@ -39,11 +53,17 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
         vim.bo.filetype = "nu"
     end
 })
+-- vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+--     pattern = "*.c3",
+--     callback = function()
+--         vim.bo.filetype = "c3"
+--     end
+-- })
 vim.lsp.enable('nushell')
 vim.lsp["tinymist"] = {
-    cmd = { "tinymist" },
-    filetypes = { "typst" },
-    settings = {}
+  cmd = { "tinymist" },
+  filetypes = { "typst" },
+  settings = {}
 }
 vim.lsp.enable("tinymist")
 
@@ -52,13 +72,13 @@ require("config.lazy")
 -- plugin setups
 require("mason").setup()
 require("telescope").setup({
-	defaults = {
-		file_ignore_patterns = {
-			"target",
-			"node_modules",
-			"build",
-		},
-	},
+  defaults = {
+    file_ignore_patterns = {
+      "target",
+      "node_modules",
+      "build",
+    },
+  },
 })
 require("lsp_lines").setup()
 require("mini.files").setup({})
@@ -72,6 +92,7 @@ vim.lsp.config('expert', {
   root_markers = { 'mix.exs', '.git' },
   filetypes = { 'elixir', 'eelixir', 'heex' },
 })
+vim.lsp.enable 'expert'
 vim.lsp.enable("ols")
 vim.lsp.config("omnisharp", {})
 vim.lsp.enable("omnisharp")
@@ -80,26 +101,28 @@ vim.lsp.config["tsserver"] = {
     filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
   --   root_dir = function(fname)
   --       return vim.fs.dirname(
-		-- 	vim.fs.find(
-		-- 		{ "tsconfig.json", "package.json", ".git" },
-		-- 		{ upward = true, path = fname }
-		-- 	)[1]
-		-- )
+    --   vim.fs.find(
+    --     { "tsconfig.json", "package.json", ".git" },
+    --     { upward = true, path = fname }
+    --   )[1]
+    -- )
   --   end,
 }
 vim.lsp.enable("tsserver")
 
-vim.lsp.enable 'expert'
 
 vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
 
 -- copilot
 vim.g.copilot_no_tab_map = true
 vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<CR>")', {
-	expr = true,
-	replace_keycodes = false,
+  expr = true,
+  replace_keycodes = false,
 })
 vim.cmd("Copilot disable") -- disable by default
+
+vim.cmd("set list")
+vim.cmd("set listchars=tab:▷▷⋮")
 
 -- theme
 vim.cmd("colorscheme tokyonight-night")
@@ -110,7 +133,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fk", builtin.live_grep, { desc = "Telescope grep" })
 vim.keymap.set("n", "<leader>h", function()
-	vim.lsp.buf.hover()
+  vim.lsp.buf.hover()
 end, { desc = "Show lsp hover info" })
 vim.keymap.set("n", "<leader>e", MiniFiles.open)
 vim.keymap.set("n", "<leader>c", function()
